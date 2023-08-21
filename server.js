07bde3aded486a66a37ea0d2732ee6f70470dde2
Amogh-Kalashetty <139551614+Amogh-Kalashetty@users.noreply.github.com>
2023-08-21 21:03:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/contact', async (req, res) => {
+
+    const { name, email } = req.body;
+    const contact = { name, email };
+
+    await contactsCollection.insertOne(contact);
+
+    res.redirect('/');
+});
 
 app.get('/', (req, res) => {
     res.render('index');
